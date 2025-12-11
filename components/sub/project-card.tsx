774fe2945dcd72507c7031f70ce3e0e067995add
MeZoU <meziane1502@gmail.com>
2025-12-11 20:01:36 +0100

    fix: Remove links from project cards

--- components/sub/project-card.tsx
+++ components/sub/project-card.tsx
@@ -6,20 +6,16 @@ type ProjectCardProps = {
   src: string;
   title: string;
   description: string;
-  link: string;
+  link?: string;
 };
 
 export const ProjectCard = ({
   src,
   title,
   description,
-  link,
 }: ProjectCardProps) => {
   return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noreferrer noopener"
+    <div
       className="relative overflow-hidden rounded-lg shadow-lg border border-[#f9731630] hover:border-[#f97316] transition-all duration-300 hover:shadow-[0_0_30px_rgba(249,115,22,0.2)] group"
     >
       <Image
@@ -34,6 +30,6 @@ export const ProjectCard = ({
         <h1 className="text-2xl font-semibold text-white group-hover:text-[#f97316] transition-colors">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
       </div>
-    </a>
+    </div>
   );
 };
